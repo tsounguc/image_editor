62809279fb3890d162e334a5a5759ff9294b1c34
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-07 16:25:06 -0500

    create home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: IconButton(
+          icon: const Icon(Icons.upload_file),
+          onPressed: () async{
+            XFile? file = await ImagePicker().pickImage(source: ImageSource.gallery,);
+            if(file != null){
+              
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
