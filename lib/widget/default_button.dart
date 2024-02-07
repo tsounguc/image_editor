c3eeba7c23a507b3e10efb5585987af7094073a6
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-07 17:18:36 -0500

    Add action buttons on alert dialog

---
+++ lib/widget/default_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class DefaultButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Widget child;
+  final Color color;
+  final Color textColor;
+  const DefaultButton({
+    Key? key,
+    required this.onPressed,
+    required this.child,
+    required this.color,
+    required this.textColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll<Color>(color),
+        textStyle: MaterialStatePropertyAll<TextStyle>(
+          TextStyle(
+            color: textColor,
+          ),
+        ),
+      ),
+      onPressed: onPressed, child: child,
+    );
+  }
+}
--- lib/widget/edit_image_viewmodel.dart
+++ lib/widget/edit_image_viewmodel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_editor/screens/edit_image_screen.dart';
+import 'package:image_editor/widget/default_button.dart';
 
 abstract class EditImageViewModel extends State<EditImageScreen> {
   TextEditingController controller = TextEditingController();
@@ -9,7 +10,7 @@ abstract class EditImageViewModel extends State<EditImageScreen> {
       builder: (context) => AlertDialog(
         title: const Text('Add New Text'),
         content: TextField(
-          controller: controller, 
+          controller: controller,
           maxLines: 5,
           decoration: const InputDecoration(
             suffixIcon: Icon(
@@ -19,6 +20,20 @@ abstract class EditImageViewModel extends State<EditImageScreen> {
             hintText: 'Your Text Here...',
           ),
         ),
+        actions: <Widget>[
+          DefaultButton(
+            onPressed: () => Navigator.of(context).pop(),
+            color: Colors.white,
+            textColor: Colors.black,
+            child: const Text('Back'),
+          ),
+          DefaultButton(
+            onPressed: () {},
+            color: Colors.red,
+            textColor: Colors.white,
+            child: const Text('Add Text'),
+          )
+        ],
       ),
     );
   }
