ce44552782759d75e6c10ebf6909687942f7eece
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-07 16:29:01 -0500

    Navigate to edit image screen

---
+++ lib/screens/edit_image_screen.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class EditImageScreen extends StatefulWidget {
+  final String selectedImage;
+  const EditImageScreen({Key? key, required this.selectedImage})
+      : super(key: key);
+
+  @override
+  State<EditImageScreen> createState() => _EditImageScreenState();
+}
+
+class _EditImageScreenState extends State<EditImageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Image.file(
+        File(widget.selectedImage),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_editor/screens/edit_image_screen.dart';
 import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,10 +11,18 @@ class HomeScreen extends StatelessWidget {
       body: Center(
         child: IconButton(
           icon: const Icon(Icons.upload_file),
-          onPressed: () async{
-            XFile? file = await ImagePicker().pickImage(source: ImageSource.gallery,);
-            if(file != null){
-              
+          onPressed: () async {
+            XFile? file = await ImagePicker().pickImage(
+              source: ImageSource.gallery,
+            );
+            if (file != null) {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => EditImageScreen(
+                    selectedImage: file.path,
+                  ),
+                ),
+              );
             }
           },
         ),
